de317eecf15e3de004a4c0f416253e78f75787d5
gommjabbar <gommjabbar@gmail.com>
2015-06-30 16:32:56 +0300

    BaseEntity

---
+++ Sinq/Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sinq.Models
+{
+    public class BaseEntity
+    {
+        public DateTimeOffset CreateDate { get; set; }
+        public DateTimeOffset? UpdateDate { get; set; }
+    }
+}
