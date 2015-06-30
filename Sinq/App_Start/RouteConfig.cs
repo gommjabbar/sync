f7af826bfb89986c8cc61fced7d87f6cd63858a8
gommjabbar <gommjabbar@gmail.com>
2015-06-30 12:24:56 +0300

    Activity MVC scaffolded stuff

--- Sinq/App_Start/RouteConfig.cs
+++ Sinq/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Sinq
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Activities", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
