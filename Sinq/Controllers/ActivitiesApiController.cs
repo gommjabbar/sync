e294be1236fc883a5889e6c1604640dff7c1b064
gommjabbar <gommjabbar@gmail.com>
2015-07-01 15:47:19 +0300

    activities Api controller and add request

---
+++ Sinq/Controllers/ActivitiesApiController.cs
@@ -0,0 +1,38 @@
+﻿using Sinq.Models;
+using Sinq.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sinq.Controllers
+{
+    public class ActivitiesApiController : ApiController
+    {
+         private IActivityRepository activityRepository;
+
+         public ActivitiesApiController()
+         {
+
+             this.activityRepository = new ActivityRepository(new SyncDbContext());
+       }
+        [Route("api/activities")]
+        [HttpPost]
+         public Activity Create(Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                //in metoda update din repository
+                //db.Entry(activity).State = EntityState.Modified;
+                activityRepository.Add(activity);
+
+                //db.SaveChanges();
+                activityRepository.SaveChanges();
+                return activity;
+            }
+            return null;
+        }
+    }
+}
