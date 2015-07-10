59246e421a48f2dc92eaad62a937410e131717e6
AlecsandraCostea <alecsandra.costea@yahoo.com>
2015-07-10 11:05:31 +0300

    activity details display function

--- Sinq/Scripts/components/activity-details-component.js
+++ Sinq/Scripts/components/activity-details-component.js
@@ -3,7 +3,15 @@
         var self = this;
         self.SelectedActivity = params.SelectedActivity || ko.observable(new Activity({}));
         self.ShowDetails = ko.observable();
+        self.DisplayActivityDetails = ko.observable(false);
 
+        self.SelectedActivity.subscribe(function (newSelectedActivity) {
+            self.fnShowActivityDetails();
+        })
+
+        self.fnShowActivityDetails = function () {
+            self.DisplayActivityDetails(true);
+        }
 
         //The function removes an activity
         self.fnRemoveActivity = function () {
